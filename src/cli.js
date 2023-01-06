aad29b26b405496ab0e3b83dbe9174cb507202be
Artem <selfex9496@gmail.com>
2023-01-06 21:13:55 +0700

    fix files

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,10 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const greetAndAskForName = () => {
+  console.log('Welcome to the Brain Games!');
 
-const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
-export default name;
+export default greetAndAskForName;
