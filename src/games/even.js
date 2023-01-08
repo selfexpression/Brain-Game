280b0b3292d35c3cb044c9f10fb5620786f08dba
Artem <selfex9496@gmail.com>
2023-01-08 21:16:53 +0700

    fix

--- src/games/even.js
+++ src/games/even.js
@@ -10,21 +10,18 @@ export default () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   for (let i = 0; i < 3; i += 1) {
-    const getRandomNum = () => Math.ceil(Math.random() * 100);
-    const num = getRandomNum();
-    const evenNum = num % 2 === 0;
+    const getRandomNum = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
+    const questionNum = getRandomNum(0, 100);
+    const isEven = () => questionNum % 2 === 0;
 
-    const que = `Question: ${num}`;
+    const que = `Question: ${questionNum}`;
     console.log(que);
 
     const answerUser = readlineSync.question('Your answer: ');
-
-    const checking1 = (answerUser === 'yes') && (evenNum);
-    const checking2 = (answerUser === 'no') && (!evenNum);
-    const correctAnswer = (answerUser === 'yes' ? 'no' : 'yes');
+    const correctAnswer = (isEven(questionNum) ? 'yes' : 'no');
     const errorMessage = `${answerUser} is wrong answer ;(. Correct answer was ${correctAnswer}. Let's try again, ${userName}`;
 
-    if (checking1 === true || checking2 === true) {
+    if (answerUser === correctAnswer) {
       console.log('Correct!');
     } else {
       console.log(errorMessage);
