712f0028ed8c23b315b5fcdb8e09b5beb1215f77
Artem <selfex9496@gmail.com>
2023-01-11 00:41:48 +0700

    fix

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,7 @@ import { getRandomNum, isEvenNum } from '../utils.js';
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getQuestionAndAnswer = () => {
-  const question = getRandomNum();
+  const question = getRandomNum(0, 100);
   const correctAnswer = (isEvenNum(question) ? 'yes' : 'no');
 
   return [question, correctAnswer];
