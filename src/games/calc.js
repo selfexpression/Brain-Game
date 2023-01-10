4c14273da66e31095dea5aadc250e8d566968fc2
Artem <selfex9496@gmail.com>
2023-01-10 21:17:50 +0700

    fix

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,11 +1,48 @@
 import generalLogic from '../index.js';
-import { getRandomExp, getResultExp } from '../utils.js';
+import { getRandomNum } from '../utils.js';
 
 const description = 'What is the result of the expression?';
 
+const getRandomOperationSymbol = (symbols) => {
+  const randomIndex = getRandomNum(0, symbols.length);
+  const randomSymbol = symbols[randomIndex];
+  return randomSymbol;
+};
+
 const getQuestionAndAnswer = () => {
-  const question = getRandomExp();
-  const correctAnswer = String(getResultExp(question));
+  const operand1 = getRandomNum(0, 20);
+  const operand2 = getRandomNum(0, 20);
+
+  const expressions = [
+    [`${operand1} + ${operand2}`],
+    [`${operand1} - ${operand2}`],
+    [`${operand1} * ${operand2}`],
+  ];
+
+  const operators = [
+    ['+'],
+    ['-'],
+    ['*'],
+  ];
+
+  const question = getRandomOperationSymbol(expressions);
+  const operator = getRandomOperationSymbol(operators);
+  const middleChar = question.length / 2;
+
+  let correctAnswer;
+
+  switch (question.charAt(middleChar) === operator) {
+    case '+':
+      correctAnswer = operand1 + operand2;
+      break;
+    case '-':
+      correctAnswer = operand1 + operand2;
+      break;
+    case '*':
+      correctAnswer = operand1 + operand2;
+      break;
+    default:
+  }
 
   return [question, correctAnswer];
 };
--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,3 @@
-export const getRandomNum = () => Math.ceil(Math.random() * 50);
+export const getRandomNum = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
 
 export const isEvenNum = (num) => num % 2 === 0;
-
-export const getRandomExp = () => {
-  const expressionsArray = [
-    [`${getRandomNum()} + ${getRandomNum()}`],
-    [`${getRandomNum()} - ${getRandomNum()}`],
-    [`${getRandomNum()} * ${getRandomNum()}`],
-  ];
-  const randomIndex = Math.ceil(Math.random() * (expressionsArray.length - 1));
-  const result = expressionsArray[randomIndex];
-
-  return result;
-};
-
-export const getResultExp = (string) => {
-  const stringToArray = string.split(' ');
-  const [operand1, operator, operand2] = stringToArray;
-
-  switch (operator) {
-    case '+':
-      return (Number(operand1) + Number(operand2));
-    case '-':
-      return (Number(operand1) - Number(operand2));
-    case '*':
-      return (Number(operand1) * Number(operand2));
-    default:
-      return null;
-  }
-};
