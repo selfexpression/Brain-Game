2a9f71f4875ea5b8cf72547ad42f3d12b6884963
Artem <selfex9496@gmail.com>
2023-01-11 12:51:21 +0700

    add new progression game files

---
+++ src/games/progression.js
@@ -0,0 +1,11 @@
+import generalLogic from '../index.js';
+
+const description = 'What number is missing in the progression?';
+
+const getQuestionAndAnswer = () => {
+
+};
+
+export default () => {
+  generalLogic(description, getQuestionAndAnswer);
+};
